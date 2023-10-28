efe6136866cfff24bf255d8e3fb60a0c15fd71ee
Erick-Sagastume <139666256+Erick-Sagastume@users.noreply.github.com>
2023-10-27 22:43:32 -0600

    Update

--- agua/settings.py
+++ agua/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'agua.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'agua',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'POST':'3306',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
     }
 }
 
